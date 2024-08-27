ad39ae62a23c49cdb889903bf9a323b5eff1a364
Amit Singh <cse.amitallenhouse@gmail.com>
2024-08-27 09:06:03 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    echo "Welcome to PantherSchools"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PantherSchools</title>
+</head>
+<body>
+    <h1>PantherSchools</h1>
+</body>
+</html>
